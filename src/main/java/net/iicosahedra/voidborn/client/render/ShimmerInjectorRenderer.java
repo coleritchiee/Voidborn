6fff79e4a52d2ce14e2f00377287b0857a902a4f
coleritchiee <critchi1@binghamton.edu>
2025-03-21 05:30:08 -0400

    it renders kinda

--- src/main/java/net/iicosahedra/voidborn/client/render/ShimmerInjectorRenderer.java
+++ src/main/java/net/iicosahedra/voidborn/client/render/ShimmerInjectorRenderer.java
@@ -4,20 +4,26 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.iicosahedra.voidborn.Voidborn;
 import net.iicosahedra.voidborn.item.ShimmerInjectorItem;
+import net.iicosahedra.voidborn.setup.Registration;
 import net.iicosahedra.voidborn.util.ResourceLoc;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.client.event.ScreenEvent;
+import org.codehaus.plexus.util.dag.Vertex;
 import org.joml.Matrix4f;
 import software.bernie.geckolib.animation.AnimationState;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
@@ -27,12 +33,15 @@ import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 import software.bernie.geckolib.renderer.GeoRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
 
 public class ShimmerInjectorRenderer extends GeoArmorRenderer<ShimmerInjectorItem> implements ICurioRenderer {
     public ShimmerInjectorRenderer() {
         super(new DefaultedItemGeoModel<>(ResourceLocation.fromNamespaceAndPath(Voidborn.MODID,"curio/shimmer_injector")));
+        addRenderLayer(new AutoGlowingGeoLayer<>(this));
     }
 
     @Override
@@ -42,6 +51,8 @@ public class ShimmerInjectorRenderer extends GeoArmorRenderer<ShimmerInjectorIte
 
     @Override
     public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack, SlotContext slotContext, PoseStack poseStack, RenderLayerParent<T, M> renderLayerParent, MultiBufferSource renderTypeBuffer, int light, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-        defaultRender(poseStack,(ShimmerInjectorItem) stack.getItem(), renderTypeBuffer,RenderType.armorCutoutNoCull(this.getTextureLocation((ShimmerInjectorItem) stack.getItem())),renderTypeBuffer.getBuffer(RenderType.armorCutoutNoCull(this.getTextureLocation((ShimmerInjectorItem) stack.getItem()))), netHeadYaw, partialTicks, light);
+        prepForRender(slotContext.entity(), stack, EquipmentSlot.CHEST, (HumanoidModel<?>) renderLayerParent.getModel(), renderTypeBuffer, partialTicks, limbSwing, limbSwingAmount, netHeadYaw, headPitch);
+        VertexConsumer consumer = renderTypeBuffer.getBuffer(RenderType.armorCutoutNoCull(this.getTextureLocation((ShimmerInjectorItem) stack.getItem())));
+        this.renderToBuffer(poseStack, consumer, light, OverlayTexture.NO_OVERLAY);
     }
 }
--- src/main/java/net/iicosahedra/voidborn/setup/ClientSetup.java
+++ src/main/java/net/iicosahedra/voidborn/setup/ClientSetup.java
@@ -1,15 +1,23 @@
 package net.iicosahedra.voidborn.setup;
 
 import net.iicosahedra.voidborn.Voidborn;
+import net.iicosahedra.voidborn.client.render.ShimmerInjectorRenderer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
+import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
 
 @EventBusSubscriber(value = Dist.CLIENT, modid = Voidborn.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class ClientSetup {
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        CuriosRendererRegistry.register(Registration.SHIMMER_INJECTOR.get(), ShimmerInjectorRenderer::new);
+    }
+
     @SubscribeEvent
     public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event){
     }
